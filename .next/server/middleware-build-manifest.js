c1c0407c939f17d7a520d476052d01f7766c1800
khaledmagde1811 <kelshaar3@gmail.com>
2026-05-11 13:19:22 +0200

    update project

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -29,11 +29,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/tv/[id].js"
-    ],
-    "/tvshows": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/tvshows.js"
     ]
   },
   "ampFirstPages": []
